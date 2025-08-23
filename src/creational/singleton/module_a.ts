bc82062b7de1c5cc6d4f1d7368e3d3485a6fa887
Higor Araujo <higorribeiroaraujo007@gmail.com>
2025-08-23 15:22:08 -0300

    feat: singleton

---
+++ src/creational/singleton/module_a.ts
@@ -0,0 +1,10 @@
+import { MyDatabaseFunction } from './db/my-database-function';
+
+const myDatabaseClassic = MyDatabaseFunction;
+myDatabaseClassic.add({ name: 'Luiz', age: 30 });
+myDatabaseClassic.add({ name: 'Maria', age: 50 });
+myDatabaseClassic.add({ name: 'Eduardo', age: 25 });
+myDatabaseClassic.remove(1);
+myDatabaseClassic.show();
+
+export { myDatabaseClassic };
