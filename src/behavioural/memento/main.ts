24b037f1b9e16b74c0d88561e0ef0a269fb21ad3
Higor <higorribeiroaraujo007@gmail.com>
2025-08-30 16:41:22 -0300

    feat: memento

---
+++ src/behavioural/memento/main.ts
@@ -0,0 +1,18 @@
+import { ImageEditor } from "./image-editor";
+import { ImageEditorBackupManager } from "./image-editor-backup-manager";
+
+const imageEditor = new ImageEditor('/media/image.png', 'png');
+const backupManager = new ImageEditorBackupManager(imageEditor);
+
+backupManager.backup();
+imageEditor.convertFormatTo('gif');
+
+backupManager.backup();
+imageEditor.convertFormatTo('bmp');
+
+backupManager.backup();
+imageEditor.convertFormatTo('jpg'); // esta alteração será desfeita
+
+backupManager.undo();
+console.log(imageEditor);
+
